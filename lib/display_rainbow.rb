9cc58be8ffedcbac69809c44e7179c13bc7d4970
Celal Cankaya <celalcankaya@outlook.com>
2019-07-07 20:32:08 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts colors[0][0].uppercase + ": " + colors[0] + ", " 
+  puts colors[0][0].uppercase + ": " + colors[0] + ", "
   + colors[1][0].uppercase + ": " + colors[1] + ", "
   + colors[2][0].uppercase + ": " + colors[2] + ", "
   + colors[3][0].uppercase + ": " + colors[3] + ", "
